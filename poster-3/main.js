724ba9a3b1c4e86f83c1554d865d81f0a1cacf87
tamsyn01 <tamsyn205501@gmail.com>
2022-03-25 09:50:58 +1100

    update wk4 class

--- poster-3/main.js
+++ poster-3/main.js
@@ -1,13 +1,23 @@
 $(function(){
     console.log("I'm ready now");
     
-    var classes = ["one", "two", "three"];
+    var num = ["one", "two", "three"];
+    var abc = ["a", "b", "c"];
+    var xyz = ["x", "y", "z"];
 
     function randomClass() {
         $(".text").each(function () {
-            $(this).removeClass(classes);
-            $(this).addClass(classes[Math.floor(Math.random() * classes.length)]);
+            $(this).removeClass(num);
+            $(this).addClass(num[Math.floor(Math.random() * num.length)]);
         });
+        $(".text").each(function () {
+            $(this).removeClass(abc);
+            $(this).addClass(abc[Math.floor(Math.random() * abc.length)]);
+        });
+        $(".text").each(function () {
+            $(this).removeClass(xyz);
+            $(this).addClass(xyz[Math.floor(Math.random() * xyz.length)]);
+        });                
         setTimeout(randomClass, 1500)
     }
 
